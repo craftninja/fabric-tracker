789af95bf3bc6bb685d945c2011723a380ceac42
EmilyPlatzer <emilyplatzer@gmail.com>
2015-06-19 10:40:00 -0600

    User can visit '/fabrics'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var bookshelf = require('./bookshelf')
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var fabrics = require('./routes/fabrics');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/fabrics', fabrics);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
